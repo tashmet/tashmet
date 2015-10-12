006f63a180dade7eca90471b49d63fe6a86f32b0
Björn Andersson <bjorn.olof.andersson@gmail.com>
2015-10-12 12:44:46 +0200

    Added caching of posts in client

--- js/components/post/post.js
+++ js/components/post/post.js
@@ -6,14 +6,32 @@ function stPostService($resource) {
   var url = "http://localhost:3001";
   var postResource = $resource(url + "/posts");
 
+  var postCache = [];
+
   return {
+    get: function(name, callback) {
+      console.log("Get: " + name);
+      this.findPosts({}, function(posts) {
+        angular.forEach(posts, function(post, index) {
+          if(post.name == name) {
+            callback(post);
+          }
+        });
+      });
+    },
+
     findPosts: function(query, callback) {
+      if(postCache.length != 0) {
+        return callback(postCache);
+      }
+
       postResource.query(query, function(posts) {
         angular.forEach(posts, function(post, index) {
           post.attachmentUrl = function(name) {
             return url + "/attachments/posts/" + post.name + "/" + name;
           }
         });
+        postCache = posts;
         callback(posts);
       });
     }
