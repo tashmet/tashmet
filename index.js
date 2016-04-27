0ac44ca85dc8ef6d59033953a9094d3caf8d8171
Björn Andersson <bjorn.olof.andersson@gmail.com>
2016-04-27 18:25:49 +0200

    Moved logging to reporter and added processPost and ready hooks

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
+var reporter = require('./lib/reporter')
 var cache    = require('samizdat-tashmetu-cache');
 var storage  = require('samizdat-tashmetu-fs');
 var express  = require('express');
-var chalk    = require('chalk');
 var _        = require('lodash');
 
-var app = null;
-var log = function() {};
-
-function loadPost(name, loading) {
+function loadPost(name, change) {
   try {
-    var post = storage.post(name, cache.schema);
-
-    if(cache.storePost(post)) {
-      log("Added post: " + chalk.cyan(name));
-    } else {
-      log("Updated post: " + chalk.cyan(name));
-    }
-  } catch(e) {
-    if(e instanceof storage.ValidationException) {
-      log(chalk.red('Failed to validate: ') + name);
-      e.errors.forEach(function(error) {
-        console.log('  ' + error.message);
-      });
-    } else if(e instanceof storage.ParseException) {
-      log(chalk.red('Failed to parse: ') + e.path);
-      console.log('  ' + e.message);
-    }
-    if(loading) {
-      process.exit();
-    }
-  }
+    cache.storePost(processPost(storage.post(name, cache.schema)));
+  } catch(e) {}
 }
 
-
-function listen(port, logger, wireup) {
-  var loading = true;
-  log = logger;
-  app = express();
+function listen(port, wireup) {
+  var app = express();
 
   app.get('/posts', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
@@ -46,7 +21,7 @@ function listen(port, logger, wireup) {
   app.get('/:post/attachments/*', function(req, res){
     var path = 'posts/' + req.params.post + '/attachments/' + req.params[0];
     res.sendFile(path, {root: './tashmetu/content'});
-  });
+  })
 
   app.get('/taxonomies/:taxonomy', function(req, res, next) {
     var tax = taxonomy.load(req.params.taxonomy);
@@ -56,24 +31,28 @@ function listen(port, logger, wireup) {
 
   wireup(app, storage, cache);
 
+  reporter(this, storage, cache);
+
   storage.watch('schemas', {
     add: function(name) {
       var obj = storage.schema(name);
       cache.storeSchema(obj);
-      log('Added schema: ' + chalk.cyan(obj.id));
-    }
+    },
   });
 
   storage.watch('posts', {
-    add:    function(name) { loadPost(name, loading); },
-    change: function(name) { loadPost(name, loading); },
+    add:    function(name) { loadPost(name, false); },
+    change: function(name) {
+      loadPost(name, true);
+    },
   });
 
   storage.ready(function() {
     setupPostRelationships(cache.posts());
     app.listen(port);
-    loading = false;
-    log("Tashmetu listens on port " + chalk.magenta(port));
+    readyListeners.forEach(function(callback) {
+      callback(port);
+    });
   });
 
   storage.listen();
@@ -95,7 +74,6 @@ function findRelatedPosts(post, rest, compare) {
 }
 
 function setupPostRelationships(posts) {
-  log("Setting up post relationships");
   posts.forEach(function(obj, index) {
     var rest = posts.slice(0);
     rest.splice(index, 1);
@@ -108,7 +86,16 @@ var comparePosts = function(a, b) {
   return _.intersection(a.tags, b.tags).length;
 }
 
+// Default post processing funtion
+var processPost = function(post) {
+  return post;
+}
+
+var readyListeners = [];
+
 module.exports = {
   listen: listen,
   comparePosts: function(compare) { comparePosts = compare; },
+  processPost: function(process) { processPost = process; },
+  ready: function(callback) { readyListeners.push(callback); },
 }
