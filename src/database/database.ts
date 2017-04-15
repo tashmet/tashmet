a836e933ee133da4c454b831ec52dee31154863e
Björn Andersson <bjorn.olof.andersson@gmail.com>
2017-04-15 16:17:23 +0200

    Proper activation of collection controllers

--- src/database/database.ts
+++ src/database/database.ts
@@ -16,18 +16,11 @@ export class DatabaseService extends EventEmitter implements Database
   private collections: {[name: string]: Collection} = {};
   private syncedCount = 0;
 
-  public constructor(
-    @inject('tashmetu.DatabaseConfig') private dbConfig: DatabaseConfig,
-    @inject('tashmetu.LocalDatabase') private localDB: LocalDatabase,
-    @inject('tashmetu.RemoteDatabase') private remoteDB: RemoteDatabase,
-    @inject('tashmetu.RoutineAggregator') private routineAggregator: RoutineAggregator,
-    @inject('tiamat.Injector') private injector: Injector
-  ) {
-    super();
-    dbConfig.mappings.forEach((mapping: CollectionMapping) => {
-      this.createCollectionController(mapping);
-    });
-  }
+  @inject('tashmetu.DatabaseConfig') private dbConfig: DatabaseConfig;
+  @inject('tashmetu.LocalDatabase') private localDB: LocalDatabase;
+  @inject('tashmetu.RemoteDatabase') private remoteDB: RemoteDatabase;
+  @inject('tashmetu.RoutineAggregator') private routineAggregator: RoutineAggregator;
+  @inject('tiamat.Injector') private injector: Injector;
 
   public collection(name: string): Collection {
     return this.collections[name];
@@ -42,48 +35,55 @@ export class DatabaseService extends EventEmitter implements Database
     return document;
   }
 
-  private createCollectionController(mapping: CollectionMapping) {
-    let source: Collection;
-    if (typeof mapping.source === 'string') {
-      source = this.injector.get<Collection>(mapping.source);
-    } else {
-      source = mapping.source(this.injector);
-    }
-    let controller = this.injector.get<CollectionController>(mapping.name);
-    let meta = Reflect.getOwnMetadata('tashmetu:collection', controller.constructor);
-    let collection = this.localDB.createCollection(meta.name);
+  @activate('tashmetu.Collection')
+  private activateCollectionController(collection: CollectionController): CollectionController {
+    let providerMeta = Reflect.getOwnMetadata('tiamat:provider', collection.constructor);
+    let meta = Reflect.getOwnMetadata('tashmetu:collection', collection.constructor);
+    this.dbConfig.mappings.forEach((mapping: CollectionMapping) => {
+      if (mapping.name === providerMeta.for) {
+        let source: Collection;
+        if (typeof mapping.source === 'string') {
+          source = this.injector.get<Collection>(mapping.source);
+        } else {
+          source = mapping.source(this.injector);
+        }
+        collection.setSource(source);
+      }
+    });
+    let cache = this.localDB.createCollection(meta.name);
     let buffer = this.localDB.createCollection(meta.name + ':buffer');
-    let routines = this.routineAggregator.getRoutines(controller);
-    controller.setCache(collection);
-    controller.setBuffer(buffer);
-    controller.setSource(source);
+    let routines = this.routineAggregator.getRoutines(collection);
+    collection.setCache(cache);
+    collection.setBuffer(buffer);
     routines.forEach((routine: any) => {
-      routine.setController(controller);
-      controller.addRoutine(routine);
+      routine.setController(collection);
+      collection.addRoutine(routine);
     });
 
     if (this.isServer()) {
-      controller.populate();
+      collection.populate();
     }
 
-    controller.on('ready', () => {
+    collection.on('ready', () => {
       this.syncedCount += 1;
       if (this.syncedCount === Object.keys(this.collections).length) {
         this.emit('database-synced');
       }
     });
 
-    controller.on('document-upserted', (doc: any) => {
-      this.emit('document-upserted', doc, controller);
+    collection.on('document-upserted', (doc: any) => {
+      this.emit('document-upserted', doc, collection);
     });
-    controller.on('document-removed', (doc: any) => {
-      this.emit('document-removed', doc, controller);
+    collection.on('document-removed', (doc: any) => {
+      this.emit('document-removed', doc, collection);
     });
-    controller.on('document-error', (err: any) => {
-      this.emit('document-error', err, controller);
+    collection.on('document-error', (err: any) => {
+      this.emit('document-error', err, collection);
     });
 
-    this.collections[meta.name] = controller;
+    this.collections[meta.name] = collection;
+
+    return collection;
   }
 
   private isServer() {
