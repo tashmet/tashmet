c465d14a96494e9fccb4da2764eac8ed2659e836
Björn Andersson <bjorn.olof.andersson@gmail.com>
2017-06-11 13:03:08 +0200

    Changed 'let' to 'const' in range tests

--- test/database/cache/range.spec.ts
+++ test/database/cache/range.spec.ts
@@ -5,37 +5,37 @@ import 'mocha';
 describe('Range', () => {
   describe('containment', () => {
     it('should contain itself', () => {
-      let r = new Range(0, 2);
+      const r = new Range(0, 2);
 
       expect(r.contains(r)).to.equal(true);
     });
     it('should contain a shorter range with same start', () => {
-      let r1 = new Range(0, 2);
-      let r2 = new Range(0, 1);
+      const r1 = new Range(0, 2);
+      const r2 = new Range(0, 1);
 
       expect(r1.contains(r2)).to.equal(true);
     });
     it('should contain a shorter range with same end', () => {
-      let r1 = new Range(0, 2);
-      let r2 = new Range(1, 2);
+      const r1 = new Range(0, 2);
+      const r2 = new Range(1, 2);
 
       expect(r1.contains(r2)).to.equal(true);
     });
     it('should not contain a range with lower start', () => {
-      let r1 = new Range(1, 2);
-      let r2 = new Range(0, 2);
+      const r1 = new Range(1, 2);
+      const r2 = new Range(0, 2);
 
       expect(r1.contains(r2)).to.equal(false);
     });
     it('should not contain a range with higher end', () => {
-      let r1 = new Range(1, 2);
-      let r2 = new Range(1, 3);
+      const r1 = new Range(1, 2);
+      const r2 = new Range(1, 3);
 
       expect(r1.contains(r2)).to.equal(false);
     });
     it('should not contain a range fully outside', () => {
-      let r1 = new Range(1, 2);
-      let r2 = new Range(3, 4);
+      const r1 = new Range(1, 2);
+      const r2 = new Range(3, 4);
 
       expect(r1.contains(r2)).to.equal(false);
     });
@@ -43,31 +43,31 @@ describe('Range', () => {
 
   describe('overlap length', () => {
     it('should be equal to own length when overlaped with itself', () => {
-      let r = new Range(0, 2);
+      const r = new Range(0, 2);
 
       expect(r.overlapLength(r)).to.equal(3);
     });
     it('should be equal to length of smallest range when fully overlapping', () => {
-      let r1 = new Range(0, 2);
-      let r2 = new Range(1, 2);
+      const r1 = new Range(0, 2);
+      const r2 = new Range(1, 2);
 
       expect(r1.overlapLength(r2)).to.equal(2);
     });
     it('should be accurate when overlapping at beginning', () => {
-      let r1 = new Range(1, 2);
-      let r2 = new Range(0, 1);
+      const r1 = new Range(1, 2);
+      const r2 = new Range(0, 1);
 
       expect(r1.overlapLength(r2)).to.equal(1);
     });
     it('should be accurate when overlapping at end', () => {
-      let r1 = new Range(1, 2);
-      let r2 = new Range(1, 5);
+      const r1 = new Range(1, 2);
+      const r2 = new Range(1, 5);
 
       expect(r1.overlapLength(r2)).to.equal(2);
     });
     it('should be zero when ranges do not overlap', () => {
-      let r1 = new Range(1, 2);
-      let r2 = new Range(3, 5);
+      const r1 = new Range(1, 2);
+      const r2 = new Range(3, 5);
 
       expect(r1.overlapLength(r2)).to.equal(0);
     });
