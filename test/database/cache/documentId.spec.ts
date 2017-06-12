95f028f95076c8dcda4de3c761838d1388e34cf6
Björn Andersson <bjorn.olof.andersson@gmail.com>
2017-06-12 20:17:56 +0200

    Tests for DocumentIdEvaluator

---
+++ test/database/cache/documentId.spec.ts
@@ -0,0 +1,16 @@
+import {DocumentIdEvaluator} from '../../../src/database/cache/documentId';
+import {expect} from 'chai';
+import 'mocha';
+
+describe('DocumentIdEvaluator', () => {
+  let evaluator = new DocumentIdEvaluator();
+
+  it('should initially not have a given document cached', () => {
+    expect(evaluator.isCached({_id: 'foo'}, {})).to.equal(false);
+  });
+  it('should cache one document', () => {
+    expect(evaluator.add({_id: 'foo'}));
+    expect(evaluator.isCached({_id: 'foo'}, {})).to.equal(true);
+    expect(evaluator.isCached({_id: 'bar'}, {})).to.equal(false);
+  });
+});
