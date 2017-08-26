74aa95cd12afb681109746b31bd95602f8b10b86
Björn Andersson <bjorn.olof.andersson@gmail.com>
2017-08-26 17:38:16 +0200

    Allow sorting filter to be modified

--- src/database/filters/sort.ts
+++ src/database/filters/sort.ts
@@ -7,9 +7,27 @@ export function sortFilter(config: Sorting) {
   };
 }
 
-export class SortFilterImpl extends BaseFilter {
+export class SortFilterImpl extends BaseFilter implements Sorting {
   public constructor(private config: Sorting, view: View) {
     super(view);
+  }
+
+  public get key(): string {
+    return this.config.key;
+  }
+
+  public set key(k: string) {
+    this.config.key = k;
+    this.emit('filter-changed');
+  }
+
+  public get order(): SortingOrder {
+    return this.config.order;
+  }
+
+  public set order(o: SortingOrder) {
+    this.config.order = o;
+    this.emit('filter-changed');
   }
 
   public apply(selector: any, options: QueryOptions): void {
