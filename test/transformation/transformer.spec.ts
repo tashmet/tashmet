31629ea39c6ddf9b6453b4d2a1e3309b31796a54
Björn Andersson <bjorn.olof.andersson@gmail.com>
2017-09-03 23:07:22 +0200

    Test toInstance without model

--- test/transformation/transformer.spec.ts
+++ test/transformation/transformer.spec.ts
@@ -1,7 +1,11 @@
 import {TransformerService} from '../../src/transformation/transformer';
 import {model, string} from '../../src/transformation/decorators';
 import {expect} from 'chai';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
 import 'mocha';
+
+chai.use(chaiAsPromised);
 
 describe('TransformerService', () => {
   describe('toInstance', () => {
@@ -13,6 +17,12 @@ describe('TransformerService', () => {
     }
 
     const ts = new TransformerService([TestModel]);
+
+    it('should fail to transform a plain object without _model property', () => {
+      const plain = {foo: 'bar'};
+
+      expect(ts.toInstance(plain, 'persist')).to.be.rejectedWith(Error);
+    });
 
     it('should transform a plain object', () => {
       const plain = {foo: 'bar', _model: 'test.TestModel'};
