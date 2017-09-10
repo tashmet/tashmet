92bf04dca4ba3a987faa0449c0238aed84aa68ee
Björn Andersson <bjorn.olof.andersson@gmail.com>
2017-09-10 12:33:07 +0200

    Fixed bug in TypeDecorator

--- src/validation/types/common.ts
+++ src/validation/types/common.ts
@@ -2,6 +2,6 @@ import {PropertyDecorator} from '@ziggurat/tiamat';
 
 export class TypeDecorator<T> extends PropertyDecorator<T> {
   public decorate(data: T, target: any, key: string) {
-    this.appendMeta('isimud:modelProperty', key, target.constructor);
+    this.appendOwnMeta('isimud:modelProperty', key, target.constructor);
   }
 }
