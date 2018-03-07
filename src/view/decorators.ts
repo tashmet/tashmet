f3ca391183ff9a3a5ae56b82bc17a63bd9d51acf
Björn Andersson <bjorn.olof.andersson@gmail.com>
2018-03-07 20:52:51 +0100

    Use ServiceIdentifier in viewOf decorator

--- src/view/decorators.ts
+++ src/view/decorators.ts
@@ -1,4 +1,5 @@
-import {classDecorator, ClassAnnotation} from '@ziggurat/tiamat';
+import {classDecorator, ClassAnnotation, ServiceIdentifier,
+  Abstract, Newable} from '@ziggurat/tiamat';
 
-export const viewOf = classDecorator<string>(
+export const viewOf = classDecorator<ServiceIdentifier<any>>(
   new ClassAnnotation('isimud:viewOf'));
