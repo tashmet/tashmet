9b3a9072897c01095c1b1ebc8bbd7a47b0401e3c
Björn Andersson <bjorn.olof.andersson@gmail.com>
2018-03-15 13:29:20 +0100

    Better type restriction for viewOf decorator

--- src/view/decorators.ts
+++ src/view/decorators.ts
@@ -1,5 +1,6 @@
 import {classDecorator, ClassAnnotation, ServiceIdentifier,
   Abstract, Newable} from '@ziggurat/tiamat';
+import {Controller} from '../database/controller';
 
-export const viewOf = classDecorator<ServiceIdentifier<any>>(
+export const viewOf = classDecorator<ServiceIdentifier<Controller<any>>>(
   new ClassAnnotation('isimud:viewOf'));
