9f46397364e14d51ea156598e7de3c2328d1bcfe
Björn Andersson <bjorn.olof.andersson@gmail.com>
2018-05-17 15:12:23 +0200

    Better type support for inline source provider

--- src/sources/inline.ts
+++ src/sources/inline.ts
@@ -1,8 +1,9 @@
 import {Injector} from '@ziggurat/tiamat';
 import {SourceProvider} from '../database/interfaces';
 import {Collection, CollectionFactory, MemoryCollectionConfig} from '../interfaces';
+import {Document} from '../models/document';
 
-export function inline(name: string, docs: any[]): SourceProvider {
+export function inline<T extends Document>(name: string, docs: T[]): SourceProvider {
   return (injector: Injector, model: string): Collection => {
     let factory = injector.get<CollectionFactory<MemoryCollectionConfig>>(
       'isimud.MemoryCollectionFactory'
