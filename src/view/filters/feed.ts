faebde3ae25905e99affa62fcaabe2944b8cf232
Björn Andersson <bjorn.olof.andersson@gmail.com>
2018-09-03 13:29:55 +0200

    Added documentation for feed filter

--- src/view/filters/feed.ts
+++ src/view/filters/feed.ts
@@ -2,6 +2,36 @@ import {QueryOptions} from '../../interfaces';
 import {Filter, FeedConfig} from '../interfaces';
 import {View} from '../view';
 
+/**
+ * A filter that provides a feed.
+ *
+ * This filter is suited for where a list of items are shown and the user has the ability
+ * to load more. The feed will keep track of how many items should be displayed and whether or not
+ * there are more items available.
+ *
+ * @usageNotes
+ * The feed is configured by setting an initial limit and an increment by which the limit is
+ * expanded each time more items are requested.
+ *
+ * ```typescript
+ * class MyView {
+ *   feed = new FeedFilter({
+ *     limit: 10,
+ *     increment: 5
+ *   });
+ * }
+ * ```
+ * Provided that the collection has enough documents available the above feed will make sure that
+ * the view has only 10 documents initially. By calling hasMore() on the feed we can determine if
+ * there are more documents that can be loaded into the view. Calling the loadMore() method will
+ * increase the capacity to 15 documents.
+ *
+ * ```typescript
+ * if (view.feed.hasMore()) {
+ *   view.feed.loadMore()
+ * }
+ * ```
+ */
 export class FeedFilter extends Filter {
   public limit: number;
   public increment: number;
@@ -24,11 +54,19 @@ export class FeedFilter extends Filter {
     });
   }
 
+  /**
+   * Load more documents by increasing the limit with the value of the increment.
+   */
   public loadMore(): void {
     this.pendingIncrement = this.increment;
     this.dirty = true;
   }
 
+  /**
+   * Check if there are more documents in the collection currently filtered out by the feed.
+   *
+   * @returns true if there are more items available.
+   */
   public get hasMore(): boolean {
     return this._hasMore;
   }
