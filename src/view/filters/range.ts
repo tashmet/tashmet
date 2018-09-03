0c6ea4f12ac6ed5efe59efff8018357f7c5a08ae
Björn Andersson <bjorn.olof.andersson@gmail.com>
2018-09-03 14:48:32 +0200

    Added documentation for range filter

--- src/view/filters/range.ts
+++ src/view/filters/range.ts
@@ -1,6 +1,26 @@
 import {QueryOptions} from '../../interfaces';
 import {Filter, RangeConfig} from '../interfaces';
 
+/**
+ * A filter that limits the range of documents.
+ *
+ * The range filter allows us to limit the number of documents displayed in the view to a
+ * specific range of the result set.
+ *
+ * @usageNotes
+ * The range is configured by supplying a length and an optional offset. Here we create a filter
+ * that will limit the view to the first 10 matching documents in the collection. Note that
+ * omitting the offset in this case would yield the same result.
+ *
+ * ```typescript
+ * class MyView {
+ *   range = new RangeFilter({
+ *     offset: 0
+ *     length: 10
+ *   })
+ * }
+ * ```
+ */
 export class RangeFilter extends Filter {
   public offset: number;
   public length: number;
