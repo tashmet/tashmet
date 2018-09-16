985e3e4e8658db3694a69c30e21c1a46f0d72855
Björn Andersson <bjorn.olof.andersson@gmail.com>
2018-09-16 10:39:39 +0200

    Broke out view and filters to own package

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,6 @@ import {Ningal} from '@ziggurat/ningal';
 
 export {MemoryCollection} from './collections/memory';
 export {Controller} from './database/controller';
-export {View} from './view/view';
-export {FeedFilter, FeedConfig} from './view/filters/feed';
-export {RangeFilter, RangeConfig} from './view/filters/range';
-export {SortingFilter, SortingConfig} from './view/filters/sort';
-export {SelectorFilter, SelectorConfig} from './view/filters/selector';
 export {Document} from './models/document';
 export {json} from './serializers/json';
 export {inline} from './sources/inline';
@@ -16,8 +11,6 @@ export {remote} from './sources/remote';
 export * from './interfaces';
 export * from './database/decorators';
 export * from './database/interfaces';
-export * from './view/decorators';
-export * from './view/interfaces';
 
 import {DatabaseService} from './database/database';
 import {MemoryCollectionFactory} from './collections/memory';
