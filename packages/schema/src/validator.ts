2bbe3d5a65350d836da05f833987ffe021bc8a5e
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-03-03 21:07:00 +0100

    improvement(schema): include document in validation error

--- packages/schema/src/validator.ts
+++ packages/schema/src/validator.ts
@@ -1,11 +1,14 @@
 import {provider} from '@ziqquratu/core';
-import {Database} from '@ziqquratu/database';
+import {Database, DocumentError} from '@ziqquratu/database';
 import {ValidationConfig} from './interfaces';
 import Ajv from 'ajv';
 
-export class AjvError extends Error {
-  public constructor(public errors: Ajv.ErrorObject[]) {
-    super(errors[0].message);
+export class AjvError extends DocumentError {
+  public constructor(
+    doc: any,
+    public readonly errors: Ajv.ErrorObject[]
+  ) {
+    super(doc, `validation of '${doc._id}' failed: ${errors[0].message}`);
   }
 }
 
@@ -30,7 +33,7 @@ export class AjvValidator {
       throw new Error('Could not compile schema: ' + schemaId);
     }
     if (!validate(doc)) {
-      throw new AjvError(validate.errors || []);
+      throw new AjvError(doc, validate.errors || []);
     }
     return doc;
   }
