e3d22d8a8d32d768e8e6ff1b88ade4ab359f4ca6
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-03-21 21:29:54 +0100

    test(database): improved http collection tests

--- packages/database/test/collections/http.spec.ts
+++ packages/database/test/collections/http.spec.ts
@@ -31,7 +31,6 @@ describe('HttpCollection', () => {
       body: {_id: 'foo'},
       headers: {'x-total-count': '1'}
     });
-
     fetchMock.head('*', {headers: {'x-total-count': '0'}});
   });
 
@@ -41,7 +40,7 @@ describe('HttpCollection', () => {
 
   describe('count', () => {
     it('should get count from "x-total-count" header', async () => {
-      expect(col.find({_id: 'foo'}).count()).to.eventually.eql(1);
+      return expect(col.find({_id: 'foo'}).count()).to.eventually.eql(1);
     });
   });
 
@@ -49,6 +48,7 @@ describe('HttpCollection', () => {
     before(() => {
       fetchMock.post(matchBody({_id: 'bar'}), {body: {_id: 'bar', server: 'added'}});
       fetchMock.post(matchBody({_id: 'foo'}), {throws: new Error('ID already exists')});
+      fetchMock.post(matchBody({_id: 'error'}), {status: 500, body: ''});
       fetchMock.put('/api/test/foo', {body: {_id: 'foo', server: 'updated'}});
     });
 
@@ -57,21 +57,22 @@ describe('HttpCollection', () => {
     });
 
     it('should POST if document does not exist on server', async () => {
-      expect(col.insertOne({_id: 'bar'})).to.eventually.eql({_id: 'bar', server: 'added'});
+      return expect(col.insertOne({_id: 'bar'})).to.eventually.eql({_id: 'bar', server: 'added'});
     });
     it('should throw if document already exists on server', () => {
-      expect(col.insertOne({_id: 'foo'})).to.eventually.be.rejected;
+      return expect(col.insertOne({_id: 'foo'}))
+        .to.eventually.be.rejectedWith('ID already exists');
+    });
+    it('should throw if there is an error in the server', () => {
+      return expect(col.insertOne({_id: 'error'}))
+        .to.eventually.be.rejectedWith('Internal Server Error');
     });
   });
 
   describe('replaceOne', () => {
     before(() => {
-      fetchMock.get(uri('/api/test', {_id: 'foo'}, {limit: 1}), {
-        body: [{_id: 'foo'}],
-      });
-      fetchMock.get(uri('/api/test', {_id: 'bar'}, {limit: 1}), {
-        body: [],
-      });
+      fetchMock.get(uri('/api/test', {_id: 'foo'}, {limit: 1}), {body: [{_id: 'foo'}]});
+      fetchMock.get(uri('/api/test', {_id: 'bar'}, {limit: 1}), {body: []});
       fetchMock.post(matchBody({_id: 'bar'}), {body: {_id: 'bar', server: 'added'}});
       fetchMock.put('/api/test/foo', {body: {_id: 'foo', server: 'updated'}});
     });
@@ -81,13 +82,15 @@ describe('HttpCollection', () => {
     });
 
     it('should PUT if document exists on server', async () => {
-      expect(col.replaceOne({_id: 'foo'}, {})).to.eventually.eql({_id: 'foo', server: 'updated'});
+      return expect(col.replaceOne({_id: 'foo'}, {}))
+        .to.eventually.eql({_id: 'foo', server: 'updated'});
     });
     it('should return null if no document matched selector', async () => {
-      expect(col.replaceOne({_id: 'bar'}, {})).to.eventually.eql(null);
+      return expect(col.replaceOne({_id: 'bar'}, {})).to.eventually.eql(null);
     });
     it('should post when upsert is specified', async () => {
-      expect(col.replaceOne({_id: 'bar'}, {_id: 'bar'}, {upsert: true})).to.eventually.eql({_id: 'bar', server: 'added'});
+      return expect(col.replaceOne({_id: 'bar'}, {_id: 'bar'}, {upsert: true}))
+        .to.eventually.eql({_id: 'bar', server: 'added'});
     });
   });
 });
