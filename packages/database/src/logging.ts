102b415cd213f231679e93dfb9e76f6119da988a
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-03-23 00:02:33 +0100

    improvement(database): properly handle events in collection logging

--- packages/database/src/logging.ts
+++ packages/database/src/logging.ts
@@ -1,5 +1,5 @@
 import {Logger} from '@ziqquratu/core';
-import {MiddlewareFactory, Collection, ReplaceOneOptions} from './interfaces';
+import {MiddlewareFactory, Collection} from './interfaces';
 
 export class LoggingMiddlewareFactory extends MiddlewareFactory {
   public constructor() {
@@ -10,48 +10,32 @@ export class LoggingMiddlewareFactory extends MiddlewareFactory {
     return this.resolve(async (logger: Logger) => {
       logger = logger.inScope(source.name);
 
-      function log<T>(fn: (result: T, ...input: any[]) => void) {
-        return async (next: (...args: any[]) => Promise<T>, ...args: any[]) => {
-          try {
-            const result = await next(...args);
-            fn(result, ...args);
-            return result;
-          } catch (err) {
-            logger.error(err.message);
-            throw (err);
-          }
+      const logError = async (next: (...args: any[]) => Promise<any>, ...args: any[]) => {
+        try {
+          return await next(...args);
+        } catch (err) {
+          logger.error(err.message);
+          throw (err);
         }
-      }
-
-      function onInsertOne(doc: any) {
-        logger.debug(`inserted '${doc._id}'`);
-      }
-
-      function onInsertMany(docs: any[]) {
-        docs.forEach(onInsertOne);
-      }
-
-      function onReplaceOne(doc: any, selector: any, replacement: any, options: ReplaceOneOptions | undefined) {
-        logger.debug(`replaced '${doc._id}'`);
-      }
-  
-      function onDeleteOne(doc: any, selector: any) {
-        logger.debug(`deleted '${doc._id}'`);
-      }
-
-      function onDeleteMany(docs: any[], selector: any) {
-        docs.forEach(onDeleteOne);
       }
 
       return {
         methods: {
-          insertOne: log(onInsertOne),
-          insertMany: log(onInsertMany),
-          replaceOne: log(onReplaceOne),
-          deleteOne: log(onDeleteOne),
-          deleteMany: log(onDeleteMany),
+          insertOne: logError,
+          insertMany: logError,
+          replaceOne: logError,
+          deleteOne: logError,
+          deleteMany: logError,
         },
         events: {
+          'document-upserted': async (next, doc) => {
+            logger.info(`upserted '${doc._id}'`);
+            return next(doc);
+          },
+          'document-removed': async (next, doc) => {
+            logger.info(`deleted '${doc._id}'`);
+            return next(doc);
+          },
           'document-error': async (next, err) => {
             logger.error(err.message);
             return next(err);
