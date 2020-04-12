0bc2e7a1a6f90904c13383ca87a22d5e48e665c3
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-04-12 19:42:39 +0200

    test: initial integration test for markdown

---
+++ test/integration/markdown.spec.ts
@@ -0,0 +1,64 @@
+import {expect} from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import 'mocha';
+
+import * as showdown from 'showdown';
+import {markdown} from '../../packages/markdown/dist';
+import {
+  bootstrap,
+  component,
+  Provider,
+  Collection,
+  Database,
+  DatabaseConfig,
+  memory,
+} from '../../packages/ziqquratu/dist';
+
+chai.use(chaiAsPromised);
+
+describe('schema', () => {
+  @component({
+    providers: [
+      Provider.ofInstance<DatabaseConfig>('ziqquratu.DatabaseConfig', {
+        collections: {
+          'posts': {
+            source: memory({documents: [
+              {_id: 1, content: '# h1 Heading'},
+            ]}),
+            use: [
+              markdown({
+                converter: new showdown.Converter(),
+                key: 'content',
+              })
+            ],
+          }
+        },
+      })
+    ],
+    inject: ['ziqquratu.Database']
+  })
+  class TestComponent {
+    public constructor(public database: Database) {}
+  }
+
+  let collection: Collection;
+
+  before(async () => {
+    const app = (await bootstrap(TestComponent));
+    collection = await app.database.collection('posts');
+  });
+
+  describe('outgoing', () => {
+    afterEach(() => {
+      collection.removeAllListeners();
+    });
+
+    describe('findOne', () => {
+      it('should convert content to html', async () => {
+        return expect(collection.findOne({_id: 1}))
+          .to.eventually.eql({_id: 1, content: '<h1 id="h1heading">h1 Heading</h1>'});
+      });
+    });
+  });
+});
