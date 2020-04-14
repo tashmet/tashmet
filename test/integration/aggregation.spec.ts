596fc6379f7d61241b8966c8b8b93368d81ba477
Björn Andersson <bjorn.olof.andersson@gmail.com>
2020-04-14 21:58:07 +0200

    test: added more integration tests for aggregation

--- test/integration/aggregation.spec.ts
+++ test/integration/aggregation.spec.ts
@@ -16,7 +16,11 @@ import {
 
 chai.use(chaiAsPromised);
 
-const sales = [
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const salesData = [
   { _id : 1, item : 'abc', price : 10,  quantity: 2},
   { _id : 2, item : 'jkl', price : 20,  quantity: 1},
   { _id : 3, item : 'xyz', price : 5,   quantity: 10},
@@ -32,14 +36,14 @@ describe('aggregation', () => {
     providers: [
       Provider.ofInstance<DatabaseConfig>('ziqquratu.DatabaseConfig', {
         collections: {
-          sales: memory({documents: sales}),
+          sales: memory({documents: salesData}),
           totals: aggregation({
             from: 'sales',
             pipeline: [
               {
                 $group: {
                   _id : '$item',
-                  totalSaleAmount: {$sum: {$multiply: ['$price', '$quantity' ]}}
+                  totalSaleAmount: {$sum: {$multiply: ['$price', '$quantity']}}
                 }
               },
               {
@@ -56,21 +60,56 @@ describe('aggregation', () => {
     public constructor(public database: Database) {}
   }
 
+  let sales: Collection;
   let totals: Collection;
 
   before(async () => {
     const app = (await bootstrap(TestComponent));
+    sales = await app.database.collection('sales');
     totals = await app.database.collection('totals');
   });
 
-  describe('find', () => {
-    it('should have additional field', async () => {
-      return expect(totals.find().toArray())
-        .to.eventually.eql([
-          {_id : 'abc', 'totalSaleAmount': 170},
-          {_id : 'xyz', 'totalSaleAmount': 150},
-          {_id : 'def', 'totalSaleAmount': 112.5},
-        ]);
-    });
+  it('should initially have correct documents', async () => {
+    return expect(totals.find().toArray())
+      .to.eventually.eql([
+        {_id : 'abc', 'totalSaleAmount': 170},
+        {_id : 'xyz', 'totalSaleAmount': 150},
+        {_id : 'def', 'totalSaleAmount': 112.5},
+      ]);
+  });
+
+  it('should update on replace', async () => {
+    await sales.replaceOne({_id: 1}, {_id : 1, item: 'abc', price: 12, quantity: 2});
+    await sleep(20);
+
+    return expect(totals.findOne({_id: 'abc'}))
+      .to.eventually.eql({_id : 'abc', 'totalSaleAmount': 174});
+  });
+
+  it('should update on delete', async () => {
+    await sales.deleteOne({_id: 1});
+    await sleep(20);
+
+    return expect(totals.findOne({_id: 'abc'}))
+      .to.eventually.eql({_id : 'abc', 'totalSaleAmount': 150});
+  });
+
+  it('should remove no longer existing documents', async () => {
+    await sales.deleteMany({item: 'abc'});
+    await sleep(20);
+
+    return expect(totals.find().toArray())
+      .to.eventually.eql([
+        {_id : 'xyz', 'totalSaleAmount': 150},
+        {_id : 'def', 'totalSaleAmount': 112.5},
+      ]);
+  });
+
+  it('should add new documents', async () => {
+    await sales.insertOne({item: 'ghi', price: 20, quantity: 10});
+    await sleep(20);
+
+    return expect(totals.findOne({_id: 'ghi'}))
+      .to.eventually.eql({_id : 'ghi', 'totalSaleAmount': 200});
   });
 });
