321b495983fca7ad8a9d466502d1b2383a22394e
Björn Andersson <bjorn.andersson2@cevt.se>
2021-01-31 23:29:19 +0100

    improvement(nabu): removed unused interfaces

--- packages/nabu/src/interfaces.ts
+++ packages/nabu/src/interfaces.ts
@@ -1,41 +1,4 @@
-import {Factory} from '@ziqquratu/ziqquratu';
-import { IOGate } from '../../pipe/dist';
 import { ObjectPipeTransformFactory } from './pipes';
-
-/**
- * Serializer for reading and writing objects.
- */
-export interface Serializer {
-  /**
-   * Load an object from buffer.
-   */
-  deserialize(buffer: Buffer): Promise<object>;
-
-  /**
-   * Store an object to buffer.
-   */
-  serialize(data: object): Promise<Buffer>;
-}
-
-export interface Channel {
-  read(): Promise<any>;
-
-  write(data: any): Promise<any>;
-
-  on(event: 'data-updated', fn: (data: any) => void): void;
-}
-
-export abstract class SerializerFactory extends Factory<Serializer> {
-  public abstract create(): Serializer;
-}
-
-export interface Converter {
-  publish(text: string): Promise<string>;
-}
-
-export abstract class ConverterFactory extends Factory<Converter> {
-  public abstract create(): Converter;
-}
 
 export interface DirectoryConfig {
   /**
@@ -83,18 +46,4 @@ export interface FileSystemConfig {
    * @default false
    */
   watch: boolean;
-}
-
-export type ObjectMap = {[id: string]: any};
-
-export interface PersistenceAdapter {
-  write(docs: ObjectMap): Promise<void>;
-
-  read(): Promise<ObjectMap>;
-
-  remove(ids: string[]): Promise<void>;
-
-  on(event: 'document-updated', fn: (id: string, data: any) => void): PersistenceAdapter;
-
-  on(event: 'document-removed', fn: (id: string) => void): PersistenceAdapter;
 }
