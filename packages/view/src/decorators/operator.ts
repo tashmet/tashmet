468454c8e788b6914d6d5a5ef7adbe1fbdf67a18
Björn Andersson <bjorn.olof.andersson@gmail.com>
2021-08-08 18:27:02 +0200

    feat: 🎸 prefix operators with dollar sign

--- packages/view/src/decorators/operator.ts
+++ packages/view/src/decorators/operator.ts
@@ -18,48 +18,55 @@ export interface OperatorConfig {
  */
 export class Operator extends FilterAnnotation {
   /** Matches values that are equal to a specified value. */
-  static eq(config?: OperatorConfig) {
+  static $eq(config?: OperatorConfig) {
     return Operator.decorator('$eq', config);
   }
   /** Matches values that are greater than a specified value. */
-  static gt(config?: OperatorConfig) {
+  static $gt(config?: OperatorConfig) {
     return Operator.decorator('$gt', config);
   }
   /** Matches values that are greater than or equal to a specified value. */
-  static gte(config?: OperatorConfig) {
+  static $gte(config?: OperatorConfig) {
     return Operator.decorator('$gte', config);
   }
   /** Matches any of the values specified in an array. */
-  static in(config?: OperatorConfig) {
-    return Operator.decorator('$in', config);
+  static $in(config?: OperatorConfig) {
+    return Operator.decorator<any[]>('$in', config);
   }
   /** Matches values that are less than a specified value. */
-  static lt(config?: OperatorConfig) {
+  static $lt(config?: OperatorConfig) {
     return Operator.decorator('$lt', config);
   }
   /** Matches values that are less than or equal to a specified value. */
-  static lte(config?: OperatorConfig) {
+  static $lte(config?: OperatorConfig) {
     return Operator.decorator('$lte', config);
   }
   /** Matches all values that are not equal to a specified value. */
-  static ne(config?: OperatorConfig) {
+  static $ne(config?: OperatorConfig) {
     return Operator.decorator('$ne', config);
   }
   /** Matches none of the values specified in an array. */
-  static nin(config?: OperatorConfig) {
-    return Operator.decorator('$nin', config);
+  static $nin(config?: OperatorConfig) {
+    return Operator.decorator<any[]>('$nin', config);
   }
 
   /** Matches documents that have the specified field. */
-  static exists(config?: OperatorConfig) {
+  static $exists(config?: OperatorConfig) {
     return Operator.decorator<boolean>('$exists', config);
   }
   /** Selects documents if a field is of the specified type. */
-  static type(config?: OperatorConfig) {
+  static $type(config?: OperatorConfig) {
     return Operator.decorator<string>('$type', config);
   }
 
-  private static decorator<T>(operator: string, config?: OperatorConfig) {
+  static $all(config?: OperatorConfig) {
+    return Operator.decorator<any[]>('$all', config);
+  }
+  static $size(config?: OperatorConfig) {
+    return Operator.decorator<number>('$size', config);
+  }
+
+  private static decorator<T = any>(operator: string, config?: OperatorConfig) {
     return propertyDecorator<T>((target, propertyKey) => {
       const key = config?.key || propertyKey;
 
--- packages/view/test/decorators/operator.spec.ts
+++ packages/view/test/decorators/operator.spec.ts
@@ -3,7 +3,7 @@ import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
 import 'mocha';
-import {makeSelector} from '../../src/query';
+import {makeSelector, Query} from '../../src/query';
 import {Operator} from '../../src/decorators/operator';
 
 chai.use(chaiAsPromised);
@@ -12,7 +12,7 @@ chai.use(sinonChai);
 describe('Operator', () => {
   it('should apply single operator', async () => {
     class TestQuery {
-      @Operator.eq() foo: string = 'bar'
+      @Operator.$eq() foo: string = 'bar'
     }
 
     const selector = makeSelector(new TestQuery());
@@ -21,8 +21,8 @@ describe('Operator', () => {
 
   it('should apply multiple operators on the same key', async () => {
     class TestQuery {
-      @Operator.gte({key: 'foo'}) min: number = 3
-      @Operator.lte({key: 'foo'}) max: number = 7
+      @Operator.$gte({key: 'foo'}) min = 3
+      @Operator.$lte({key: 'foo'}) max = 7
     }
 
     const selector = makeSelector(new TestQuery());
