9500fb5456a6c99b52c8a77213c6cfa99dce8a22
Björn Andersson <bjorn.olof.andersson@gmail.com>
2021-09-15 17:14:28 +0200

    feat: 🎸 removed dependency on assign-deep in view

--- packages/view/src/aggregator.ts
+++ packages/view/src/aggregator.ts
@@ -1,8 +1,8 @@
 import {Annotation} from '@ziqquratu/core';
 import {AggregationPipeline} from '@ziqquratu/database';
+import {merge} from 'mingo/util';
 import {AbstractAggregator} from './interfaces';
 
-const assignDeep = require('assign-deep');
 
 export class Aggregator extends AbstractAggregator {
   public get pipeline(): AggregationPipeline {
@@ -18,7 +18,7 @@ export class Aggregator extends AbstractAggregator {
         if (pipeline.length > 0) {
           let lastStep = pipeline[pipeline.length - 1];
           if (mergeSteps.includes(op(step)) && op(step) === op(lastStep)) {
-            assignDeep(lastStep, step);
+            merge(lastStep, step as any);
             continue;
           }
         }
