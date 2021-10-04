790350c8465bd8e5acf6cc93d7936b622c5ca1e2
Björn Andersson <bjorn.olof.andersson@gmail.com>
2021-10-04 17:30:31 +0200

    feat: 🎸 alternative sort param name

--- packages/tashmetu/src/query.ts
+++ packages/tashmetu/src/query.ts
@@ -56,7 +56,6 @@ export interface MultiParamFilterParserConfig {
 }
 
 export const multiParamFilterParser = (config: MultiParamFilterParserConfig) => (req: express.Request) => {
-  // const exclude = ['sort', 'skip', 'limit', 'projection'];
   let filter: Filter<any> = {};
 
   const parseFilter = (lhs: string, rhs: string) => {
@@ -76,23 +75,24 @@ export const multiParamFilterParser = (config: MultiParamFilterParserConfig) => 
   return {filter};
 }
 
-export const singleParamSortParser = () => (req: express.Request) => {
-  const param = 'sort';
-  let sort: SortingMap = {};
-  const value = req.query[param];
-
-  if (value) {
-    for (const s of value.toString().split(',')) {
-      const sign = s.substr(0, 1);
+export const singleParamSortParser = (param: string = 'sort') => {
+  return (req: express.Request) => {
+    const sorting = (fields: string[]) => fields.reduce((sort, field) => {
+      const sign = field.substr(0, 1);
       if (sign === '-') {
-        sort[s.substr(1)] = -1;
+        sort[field.substr(1)] = -1;
       } else {
-        sort[s] = 1;
+        sort[field] = 1;
       }
-    }
-    return {sort};
+      return sort;
+    }, {} as SortingMap);
+
+    const value = req.query[param];
+
+    return value
+      ? ({sort: sorting(value.toString().split(','))})
+      : ({});
   }
-  return {};
 }
 
 export const singleParamProjectionParser = (param: string = 'projection') => {
@@ -111,6 +111,7 @@ export const singleParamProjectionParser = (param: string = 'projection') => {
 }
 
 export interface FlatQueryParserConfig {
+  sort?: string;
   projection?: string;
 }
 
@@ -118,7 +119,7 @@ export const flatQueryParser = (config?: FlatQueryParserConfig) => (req: express
   makeQuery(req, [
     multiParamFilterParser({
       exclude: [
-        'sort',
+        config?.sort || 'sort',
         'skip',
         'limit',
         config?.projection || 'projection',
