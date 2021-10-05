fc933272e9ad37e3e3103dc5b8f9d49288ec37cc
Björn Andersson <bjorn.olof.andersson@gmail.com>
2021-10-05 14:18:14 +0200

    feat: 🎸 export query serializers

--- packages/rest/src/index.ts
+++ packages/rest/src/index.ts
@@ -2,4 +2,9 @@ import {RestCollectionFactory} from './collection';
 import {RestCollectionConfig} from './interfaces';
 
 export * from './interfaces';
+export * from './query/filter';
+export * from './query/flat';
+export * from './query/json';
+export * from './query/sort';
+
 export const rest = (config: RestCollectionConfig) => new RestCollectionFactory(config);
