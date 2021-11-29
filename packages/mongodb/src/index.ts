d8ea71f4ccecaff1a5128261006f7137fb6ce948
Björn Andersson <bjorn.olof.andersson@gmail.com>
2021-11-29 14:16:25 +0100

    feat: 🎸 removed mongodb support (for now)

--- packages/mongodb/src/index.ts
+++
@@ -1,8 +0,0 @@
-import {Factory} from '@tashmit/core';
-import {CollectionFactory} from '@tashmit/database';
-import {MongoDBCollection} from './collection';
-import {MongoDBCollectionConfig} from './interfaces';
-
-export function mongodb<T = any>(config: MongoDBCollectionConfig): CollectionFactory<T> {
-  return Factory.of(({name}) => MongoDBCollection.fromConfig<T>(name, config));
-}
--- packages/mongodb/src/interfaces.ts
+++
@@ -1,7 +0,0 @@
-import mongo from 'mongodb';
-
-export interface MongoDBCollectionConfig {
-  collection: mongo.Collection;
-
-  disableEvents?: boolean;
-}
