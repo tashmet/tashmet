79a3c23122fb251960c10076b144f69f2c3114e5
Björn Andersson <bjorn.olof.andersson@gmail.com>
2022-02-17 12:44:48 +0100

    test(database): unit tests for UpdateWriter

---
+++ packages/database/test/operations/update.spec.ts
@@ -0,0 +1,67 @@
+import {UpdateWriter} from '../../dist/operations/update';
+import {MemoryDriver} from '../../dist/collections/memory';
+import {expect} from 'chai';
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import 'mocha';
+import { ChangeSet } from '../../dist';
+import operators from '@tashmit/operators/system';
+import { BasicContainer } from '@tashmit/core';
+import { DatabaseService } from '../../dist/database';
+import { DefaultLogger } from '@tashmit/core/dist/logging/logger';
+import { SimpleValidatorFactory } from '../../dist/validator';
+
+chai.use(sinonChai);
+
+const sandbox = sinon.createSandbox();
+
+const container = new BasicContainer();
+const db = new DatabaseService(
+  new DefaultLogger(), container, new SimpleValidatorFactory(), operators);
+const driver = new MemoryDriver<any>({db: 'tashmit', coll: 'test'}, db, []);
+
+
+describe('UpdateWriter', () => {
+  let writer: UpdateWriter<any>;
+  let writeSpy: sinon.SinonSpy<[cs: ChangeSet<any>], Promise<void>>;
+
+  before(() => {
+    writer = new UpdateWriter<any>(driver, false);
+  });
+
+  beforeEach(() => {
+    writeSpy = sandbox.spy(driver, 'write');
+    driver.documents = [{_id: 1}, {_id: 2}];
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should return correct result when no document was updated', () => {
+    return expect(writer.execute({filter: {_id: 3}, update: {$set: {foo: 'bar'}}}))
+      .to.eventually.eql({matchedCount: 0, modifiedCount: 0});
+  });
+
+  it('should return correct result when a document was updated', () => {
+    return expect(writer.execute({filter: {_id: 2}, update: {$set: {foo: 'bar'}}}))
+      .to.eventually.eql({matchedCount: 1, modifiedCount: 1});
+  });
+
+  it('should write correct change set to driver', async () => {
+    await writer.execute({filter: {_id: 1}, update: {$set: {foo: 'bar'}}});
+    const cs = writeSpy.getCall(0).args[0];
+    expect(cs.incoming).to.eql([{_id: 1, foo: 'bar'}]);
+    expect(cs.outgoing).to.eql([{_id: 1}]);
+  });
+
+  it('should emit change document on success', (done) => {
+    driver.on('change', ({operationType, fullDocument}) => {
+      expect(operationType).to.eql('update');
+      expect(fullDocument).to.eql({_id: 1, foo: 'bar'});
+      done();
+    });
+    writer.execute({filter: {_id: 1}, update: {$set: {foo: 'bar'}}});
+  });
+});
