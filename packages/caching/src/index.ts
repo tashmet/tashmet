6fe68294f9f45f8f6453ce7d4d83e5dc3ace9fc7
Björn Andersson <bjorn.olof.andersson@gmail.com>
2022-02-28 18:17:46 +0100

    feat(caching): use new plugin format

--- packages/caching/src/index.ts
+++ packages/caching/src/index.ts
@@ -1,16 +1,95 @@
-import {Container, Plugin, Provider} from '@tashmit/core';
-import {CachingConfig} from './interfaces';
-import {CachingLayerService} from './middleware';
-
+import {Container, Provider} from '@tashmit/core';
 export {CachingConfig};
 
-export default class CachingPlugin extends Plugin {
-  public constructor(private config: CachingConfig) {
+import {provider} from '@tashmit/core';
+import MemoryClient, {
+  CachingLayer,
+  Collection,
+  Filter,
+  Middleware,
+  QueryAggregator,
+  mutationSideEffect,
+  Database,
+} from '@tashmit/database';
+import {CachingConfig} from './interfaces';
+import {CachingCursor} from './cursor';
+import {CacheEvaluator} from './evaluator';
+import {IDCache} from './id';
+import {QueryCache} from './query';
+
+
+@provider({
+  key: CachingLayer,
+  inject: [
+    MemoryClient,
+    CachingConfig,
+  ]
+})
+export default class CachingLayerPlugin extends CachingLayer {
+  private database: Database;
+
+  public constructor(
+    client: MemoryClient,
+    private config: CachingConfig
+  ) {
     super();
+    this.database = client.db('__caching__');
   }
 
-  public register(container: Container) {
-    container.register(Provider.ofInstance(CachingConfig, this.config));
-    container.register(CachingLayerService);
+  public static configure(config: CachingConfig) {
+    return (container: Container) => {
+      container.register(Provider.ofInstance(CachingConfig, config));
+      container.register(CachingLayerPlugin);
+    }
+  }
+
+  public create<T>(collection: Collection<T>): Middleware<T> {
+    const evaluators: CacheEvaluator[] = [
+      new QueryCache(this.config.ttl),
+      new IDCache(this.config.ttl)
+    ];
+    const cache = this.database.collection(collection.name)
+
+    for (const evaluator of evaluators) {
+      const cs = cache.watch();
+      cs.on('change', ({operationType, documentKey, fullDocument}) => {
+        switch(operationType) {
+          case 'delete':
+            evaluator.remove(documentKey);
+            break;
+          case 'insert':
+          case 'replace':
+          case 'update':
+            evaluator.add(fullDocument);
+        }
+      });
+    }
+
+    return {
+      insertOne: mutationSideEffect(async (result, doc: any) => {
+        await cache.replaceOne({_id: result.insertedId}, doc);
+      }),
+      insertMany: mutationSideEffect(async (result, docs: any) => {
+        await cache.insertMany(docs)
+      }),
+      deleteOne: mutationSideEffect(async (result, filter: Filter<any>) => {
+        await cache.deleteOne(filter)
+      }),
+      deleteMany: mutationSideEffect(async (result, filter: Filter<any>) => {
+        await cache.deleteMany(filter)
+      }),
+      replaceOne: mutationSideEffect(async (result, filter: Filter<any>, replacement: any) => {
+        await cache.replaceOne(filter, replacement, {upsert: true})
+      }),
+      aggregate: next => async pipeline => {
+        const query = QueryAggregator.fromPipeline(pipeline);
+        const cursor = new CachingCursor(evaluators, cache, collection.find.bind(collection), query.filter, query.options);
+        await cursor.toArray();
+        return cache.aggregate(pipeline);
+      },
+      find: next => (filter, options) => {
+        return new CachingCursor(evaluators, cache, next, filter, options);
+      },
+    } as Middleware;
   }
 }
