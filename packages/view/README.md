4e2d00dac60bbe1fbee5f2c6e478bd699226c22b
Björn Andersson <bjorn.olof.andersson@gmail.com>
2022-03-01 12:46:12 +0100

    feat(view): removed the view package

--- packages/view/README.md
+++
@@ -1,9 +0,0 @@
-# Installation
-
-```
-npm install @tashmit/view
-```
-
-# Documentation
-
-See [https://tashmit.gitbook.io/tashmit/documentation/views](https://tashmit.gitbook.io/tashmit/documentation/views)
--- packages/view/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './decorators/operator';
-export {view} from './decorators/view';
-export {Feed} from './feed';
-export {View} from './view';
-export {Item} from './item';
-export {ItemSet} from './itemSet';
-export {ViewAggregatorAnnotation} from './aggregator';
