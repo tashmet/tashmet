220ba08349d29e92fc9b2cccbb763ead53373483
Björn Andersson <bjorn.olof.andersson@gmail.com>
2022-03-25 11:59:38 +0100

    improvement(database): removed dependency on mingo in database

--- packages/database/src/operations/update.ts
+++ packages/database/src/operations/update.ts
@@ -1,6 +1,5 @@
-import {Aggregator as MingoAggregator} from 'mingo/aggregator';
 import {makeWriteChange, ChangeSet} from '../changeSet';
-import {BulkWriteResult, Document, Store, UpdateFilter, UpdateModel, Writer} from '../interfaces';
+import {BulkWriteResult, Document, Store, UpdateModel, Writer} from '../interfaces';
 
 export class UpdateWriter<TSchema extends Document> extends Writer<TSchema, UpdateModel<TSchema>> {
   public constructor(
@@ -14,7 +13,19 @@ export class UpdateWriter<TSchema extends Document> extends Writer<TSchema, Upda
       matchedCount: input.length,
       modifiedCount: input.length, // TODO: Not necessarily true
     }
-    const output = this.updateDocuments(input, update);
+    const pipeline = Object.entries(update).reduce((acc, [k, v]) => {
+      return acc.concat([{[k]: v}]);
+    }, [] as Document[]);
+
+    if (this.single) {
+      pipeline.unshift({$limit: 1});
+    }
+
+    const output = await this.store.aggregate<TSchema>([
+      {$match: filter},
+      ...pipeline,
+    ]).toArray();
+
     await this.store.write(new ChangeSet(output, input));
 
     for (const doc of output) {
@@ -22,12 +33,5 @@ export class UpdateWriter<TSchema extends Document> extends Writer<TSchema, Upda
     }
 
     return result;
-  }
-
-  protected updateDocuments(input: TSchema[], update: UpdateFilter<TSchema>): TSchema[] {
-    const pipeline = Object.entries(update).reduce((acc, [k, v]) => {
-      return acc.concat([{[k]: v}]);
-    }, [] as Document[]);
-    return new MingoAggregator(pipeline).run(input) as TSchema[];
   }
 }
