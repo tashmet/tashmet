c860b928c967ac30e45c98996e8d44b029b0deb2
Björn Andersson <bjorn.andersson.6@volvocars.com>
2022-04-17 11:18:51 +0200

    feat(mingo): added insert command handler

---
+++ packages/mingo/src/commands/insert.ts
@@ -0,0 +1,33 @@
+import ObjectID from 'bson-objectid';
+import {Document} from '@tashmet/tashmet';
+import {MingoCommandHander} from '../command';
+
+export class InsertCommandHandler extends MingoCommandHander {
+  public execute({insert: coll, documents, ordered}: Document) {
+    let writeErrors: any[] = [];
+    let n=0;
+
+    for (let i=0; i<documents.length; i++) {
+      let error;
+
+      if (!documents[i].hasOwnProperty('_id')) {
+        documents[i]._id = new ObjectID().toHexString();
+      } else if (this.store.indexOf(coll, documents[i]) >= 0) {
+        error = {index: i, errMsg: 'Duplicate key error'};
+      }
+
+      if (error === undefined) {
+        this.store.collections[coll].push(documents[i]);
+        n++;
+      } else {
+        writeErrors.push(error);
+        if (ordered) {
+          break;
+        }
+      }
+    }
+    return writeErrors.length === 0 
+      ? {n, ok: 1}
+      : {n, ok: 1, writeErrors}
+  }
+}
