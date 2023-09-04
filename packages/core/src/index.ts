a27f9a62be60c603ee63521f6e4bf076fa05c0c2
Björn Andersson <bjorn.olof.andersson@gmail.com>
2023-09-04 12:18:19 +0200

    moved configurator to core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,69 @@
+import { BootstrapConfig, createContainer } from './ioc/bootstrap.js';
+import { Container } from './ioc/interfaces.js';
+import { Provider } from './ioc/provider.js';
+import { LogFormatter, Logger, LoggerConfig, LogLevel } from './logging/interfaces.js';
+import { Newable } from './reflection/interfaces.js';
+
 export * from './ioc/index.js';
 export * from './logging/index.js';
 export * from './reflection/index.js';
+
+export type PluginConfig = (container: Container) => void | (() => void);
+
+export interface Configuration extends LoggerConfig, BootstrapConfig {}
+
+interface Plugin<TConf> {
+  configure(conf: TConf): PluginConfig;
+}
+
+export abstract class AbstractConfigurator {
+  private plugins: PluginConfig[] = [];
+  private container: ((logger: Logger) => Container) | undefined = undefined;
+  private logLevel: LogLevel = LogLevel.None;
+  private logFormat: LogFormatter | undefined = undefined;
+
+  public constructor(
+    private providers: (Provider<any> | Newable<any>)[] = [],
+    config: Partial<Configuration>,
+  ) {
+    this.container = config.container;
+    this.logLevel = config.logLevel || LogLevel.None;
+    this.logFormat = config.logFormat;
+  }
+
+  public provide(...providers: (Provider<any> | Newable<any>)[]) {
+    this.providers.push(...providers);
+    return this;
+  }
+
+  public use<TConf>(ctr: Plugin<TConf>, conf: TConf) {
+    this.plugins.push(ctr.configure(conf));
+    return this;
+  }
+
+  protected async bootstrap<T>(app: Newable<T>): Promise<T> {
+    const container = createContainer({
+      logFormat: this.logFormat,
+      logLevel: this.logLevel,
+      container: this.container,
+    });
+
+    for (const provider of this.providers) {
+      container.register(provider);
+    }
+
+    const loaders = this.plugins.map(p => p(container));
+
+    if (!container.isRegistered(app)) {
+      container.register(app);
+    }
+
+    for (const loader of loaders) {
+      if (typeof loader === 'function') {
+        loader();
+      }
+    }
+
+    return container.resolve(app);
+  }
+}
--- packages/tashmet/src/index.ts
+++ packages/tashmet/src/index.ts
@@ -67,83 +67,29 @@ export { AggregateOptions } from './operations/aggregate.js';
 
 import {
   Container,
-  createContainer,
-  Logger,
-  LoggerConfig,
-  LogLevel,
-  LogFormatter,
   Lookup,
-  Newable,
   provider,
   Provider,
   ServiceIdentifier,
+  AbstractConfigurator,
 } from '@tashmet/core';
-import { BootstrapConfig } from '@tashmet/core';
+import { Configuration } from '@tashmet/core';
 import { Dispatcher } from '@tashmet/bridge';
 import { Database, DatabaseFactory } from './interfaces.js';
 import { DefaultDatabaseFactory } from './database.js';
 
-export interface Configuration extends LoggerConfig, BootstrapConfig {}
-
-export type PluginConfig = (container: Container) => void | (() => void);
-
-interface Plugin<TConf> {
-  configure(conf: TConf): PluginConfig;
-}
-
-export class TashmetConfigurator {
-  private providers: (Provider<any> | Newable<any>)[] = [];
-  private plugins: PluginConfig[] = [];
-
-  public constructor(
-    private container: ((logger: Logger) => Container) | undefined = undefined,
-    private logLevel: LogLevel = LogLevel.None,
-    private logFormat: LogFormatter | undefined = undefined,
-  ) {}
-
-  public provide(...providers: (Provider<any> | Newable<any>)[]) {
-    this.providers.push(...providers);
-    return this;
-  }
-
-  public use<TConf>(ctr: Plugin<TConf>, conf: TConf) {
-    this.plugins.push(ctr.configure(conf));
-    return this;
+export class TashmetConfigurator extends AbstractConfigurator {
+  public constructor(config: Partial<Configuration>) {
+    super([
+      Dispatcher,
+      DefaultDatabaseFactory,
+      Provider.ofResolver(DatabaseFactory, Lookup.of(DefaultDatabaseFactory)),
+      Tashmet,
+    ], config);
   }
 
   public async connect() {
     return this.bootstrap(Tashmet);
-  }
-
-  public async bootstrap<T>(app: Newable<T>): Promise<T> {
-    const container = createContainer({
-      logFormat: this.logFormat,
-      logLevel: this.logLevel,
-      container: this.container,
-    });
-
-    container.register(Dispatcher);
-    container.register(DefaultDatabaseFactory);
-    container.register(Provider.ofResolver(DatabaseFactory, Lookup.of(DefaultDatabaseFactory)));
-    container.register(Tashmet);
-
-    const loaders = this.plugins.map(p => p(container));
-
-    for (const provider of this.providers) {
-      container.register(provider);
-    }
-
-    if (!container.isRegistered(app)) {
-      container.register(app);
-    }
-
-    for (const loader of loaders) {
-      if (typeof loader === 'function') {
-        loader();
-      }
-    }
-
-    return container.resolve(app);
   }
 }
 
@@ -165,10 +111,6 @@ export default class Tashmet {
   }
 
   public static configure(config: Partial<Configuration> = {}) {
-    return new TashmetConfigurator(
-      config.container,
-      config.logLevel,
-      config.logFormat,
-    );
+    return new TashmetConfigurator(config);
   }
 }
